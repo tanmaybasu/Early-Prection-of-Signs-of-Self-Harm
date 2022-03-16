576f322eda5db5855afe3081bcd335990635fb79
Tanmay Basu <mailtanmaybasu@gmail.com>
2022-03-16 15:54:12 +0530

    Update selfharm_prediction.py

--- selfharm_prediction.py
+++ selfharm_prediction.py
@@ -246,7 +246,7 @@ class selfharm_prediction():
                 
         return clf,ext2,trn_model
      
-# Entropy model    
+# LogEntropy model    
      def entropy_training_model(self,trn_data,trn_cat): 
         print('\n ***** Building Entropy Based Training Model ***** \n')
         print('No of Selected Terms \t'+str(self.no_of_selected_features)) 
@@ -297,45 +297,45 @@ class selfharm_prediction():
          }     
 
 # BERT model    
-     def bert_training_model(self,trn_data,trn_cat,test_size=0.2,max_length=512): 
-        print('\n ***** Running BERT Model ***** \n')       
-        tokenizer = BertTokenizerFast.from_pretrained(self.model_source, do_lower_case=True) 
-        labels=np.asarray(trn_cat)     # Class labels in nparray format     
-
-        (train_texts, valid_texts, train_labels, valid_labels), class_names = train_test_split(trn_data, labels, test_size=test_size), trn_cat
-        train_encodings = tokenizer(train_texts, truncation=True, padding=True, max_length=max_length)
-        valid_encodings = tokenizer(valid_texts, truncation=True, padding=True, max_length=max_length)
-        train_dataset = get_torch_data_format(train_encodings, train_labels)
-        valid_dataset = get_torch_data_format(valid_encodings, valid_labels)
-        model = BertForSequenceClassification.from_pretrained(self.model_source, num_labels=len(class_names)).to("cpu")
-        training_args = TrainingArguments(
-            output_dir='./results',          # output directory
-            num_train_epochs=3,              # total number of training epochs
-            per_device_train_batch_size=16,  # batch size per device during training
-            per_device_eval_batch_size=20,   # batch size for evaluation
-            warmup_steps=500,                # number of warmup steps for learning rate scheduler
-            weight_decay=0.01,               # strength of weight decay
-            logging_dir='./logs',            # directory for storing logs
-            load_best_model_at_end=True,     # load the best model when finished training (default metric is loss)
-            logging_steps=200,               # log & save weights each logging_steps
-            evaluation_strategy="steps",     # evaluate each `logging_steps`
-            )    
-        trainer = Trainer(
-            model=model,                         # the instantiated Transformers model to be trained
-            args=training_args,                  # training arguments, defined above
-            train_dataset=train_dataset,         # training dataset
-            eval_dataset=valid_dataset,          # evaluation dataset
-            compute_metrics=self.compute_metrics,     # the callback that computes metrics of interest
-            )
-        print('\n Trainer done \n')
-        trainer.train()
-        print('\n Trainer train done \n')        
-        trainer.evaluate()
-        print('\n save model \n')
-        model_path = self.path+"bert_model"
-        model.save_pretrained(model_path)
-        tokenizer.save_pretrained(model_path)
-        return model,tokenizer,class_names
+#     def bert_training_model(self,trn_data,trn_cat,test_size=0.2,max_length=512): 
+#        print('\n ***** Running BERT Model ***** \n')       
+#        tokenizer = BertTokenizerFast.from_pretrained(self.model_source, do_lower_case=True) 
+#        labels=np.asarray(trn_cat)     # Class labels in nparray format     
+#
+#        (train_texts, valid_texts, train_labels, valid_labels), class_names = train_test_split(trn_data, labels, test_size=test_size), trn_cat
+#        train_encodings = tokenizer(train_texts, truncation=True, padding=True, max_length=max_length)
+#        valid_encodings = tokenizer(valid_texts, truncation=True, padding=True, max_length=max_length)
+#        train_dataset = get_torch_data_format(train_encodings, train_labels)
+#        valid_dataset = get_torch_data_format(valid_encodings, valid_labels)
+#        model = BertForSequenceClassification.from_pretrained(self.model_source, num_labels=len(class_names)).to("cpu")
+#        training_args = TrainingArguments(
+#            output_dir='./results',          # output directory
+#            num_train_epochs=3,              # total number of training epochs
+#            per_device_train_batch_size=16,  # batch size per device during training
+#            per_device_eval_batch_size=20,   # batch size for evaluation
+#            warmup_steps=500,                # number of warmup steps for learning rate scheduler
+#            weight_decay=0.01,               # strength of weight decay
+#            logging_dir='./logs',            # directory for storing logs
+#            load_best_model_at_end=True,     # load the best model when finished training (default metric is loss)
+#            logging_steps=200,               # log & save weights each logging_steps
+#            evaluation_strategy="steps",     # evaluate each `logging_steps`
+#            )    
+#        trainer = Trainer(
+#            model=model,                         # the instantiated Transformers model to be trained
+#            args=training_args,                  # training arguments, defined above
+#            train_dataset=train_dataset,         # training dataset
+#            eval_dataset=valid_dataset,          # evaluation dataset
+#            compute_metrics=self.compute_metrics,     # the callback that computes metrics of interest
+#            )
+#        print('\n Trainer done \n')
+#        trainer.train()
+#        print('\n Trainer train done \n')        
+#        trainer.evaluate()
+#        print('\n save model \n')
+#        model_path = self.path+"bert_model"
+#        model.save_pretrained(model_path)
+#        tokenizer.save_pretrained(model_path)
+#        return model,tokenizer,class_names
 
 # Classification of Documents
      def classification(self,trn_data,trn_cat,tst_data):  
@@ -417,9 +417,7 @@ class selfharm_prediction():
             for item in y_test:
                 actual_class_labels.append(item)
             for item in predicted:
-                predicted_class_labels.append(item)
-#        print(round(statistics.mean(probs),3))
-#        print(round(statistics.variance(probs),3))            
+                predicted_class_labels.append(item)           
         confidence_score=statistics.mean(probs)-statistics.variance(probs)
         confidence_score=round(confidence_score, 3)
         print ('\n The Probablity of Confidence of the Classifier: \t'+str(confidence_score)+'\n')        
@@ -477,4 +475,3 @@ class selfharm_prediction():
                 json.dump(tst_results, fl, ensure_ascii=False, indent=4)
             
             print('\n !!!!! Submission file with the test data class labels is ready !!!!! \n')   
-
