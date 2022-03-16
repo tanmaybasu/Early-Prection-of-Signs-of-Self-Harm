4235dc0c0a167c6a5f50f2a9139e08139f1dc73d
Tanmay Basu <mailtanmaybasu@gmail.com>
2022-03-16 15:54:38 +0530

    Update testing_selfharm_prediction.py

--- testing_selfharm_prediction.py
+++ testing_selfharm_prediction.py
@@ -2,20 +2,19 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Feb 24 22:14:36 2021
-
 @author: Tanmay Basu
 """
 
 from selfharm_prediction import selfharm_prediction
 
 
-clf=selfharm_prediction('/home/xyz/selfharm/',model='entropy',model_path='saved_models/',clf_opt='svm',no_of_selected_features=100)
-#clf=selfharm_prediction('/home/xyz/selfharm/',model='entropy',model_path='saved_models/',clf_opt='rf',no_of_selected_features=3000)
-#clf=selfharm_prediction('/home/xyz/selfharm/',model='tfidf',model_path='saved_models/',clf_opt='svm',no_of_selected_features=2500)
-#clf=selfharm_prediction('/home/xyz/selfharm/',model='doc2vec',model_path='saved_models/',clf_opt='svm',no_of_selected_features=100)
-#clf=selfharm_prediction('/home/xyz/selfharm/',model='doc2vec',model_path='saved_models/',clf_opt='rf',no_of_selected_features=100)
+clf=selfharm_prediction('/home/iiserb/tanmay/erisk2021/code/',model='entropy',model_path='saved_models/',clf_opt='svm',no_of_selected_features=100)
+#clf=selfharm_prediction('/home/iiserb/tanmay/erisk2021/code/',model='entropy',model_path='saved_models/',clf_opt='rf',no_of_selected_features=3000)
+#clf=selfharm_prediction('/home/iiserb/tanmay/erisk2021/code/',model='tfidf',clf_opt='svm',model_path='saved_models/tfidf_svm/',no_of_selected_features=2500)
+#clf=selfharm_prediction('/home/iiserb/tanmay/erisk2021/code/',model='doc2vec',model_path='saved_models/',clf_opt='svm',no_of_selected_features=100)
+#clf=selfharm_prediction('/home/iiserb/tanmay/erisk2021/code/',model='doc2vec',model_path='saved_models/',clf_opt='rf',no_of_selected_features=100)
 
-#clf=selfharm_prediction('/home/xyz/selfharm/',model='bert',model_source='bert-base-uncased')
+#clf=selfharm_prediction('/Users/basut/erisk2021/code/',model='bert',model_source='bert-base-uncased')
 
 
 clf.selfharm_prediction()
